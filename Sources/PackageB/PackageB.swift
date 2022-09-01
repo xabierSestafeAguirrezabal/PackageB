adf9896a3d461aed163028b10eebb048a3c3b4c3
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-09-01 14:39:45 +0200

    sayB

--- Sources/PackageB/PackageB.swift
+++ Sources/PackageB/PackageB.swift
@@ -1,11 +1,12 @@
 import PackageA
 
 public struct PackageB {
-    public private(set) var text = "Hello, World!"
-
     public init() {
     }
     public func callPackageAfromB(){
         A().sayPackageA()
+    }
+    public func sayPackageB(){
+        print("HELLO PACKAGE B")
     }
 }
